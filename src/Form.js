c8db37fc1620c0203b8cd0c00f0a8356a7b51be4
Ed Leijnse <ed@leijnse.info>
2021-05-03 10:02:15 +0200

    variables added to Form.js

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { feedback: '', name: 'Name', email: 'email@example.com' };
+        this.state = { feedback: '', name: 'TAGGER Biz',  from_name: 'ed.leijnse@gmail.com',  to_name: 'ed@leijnse.info', email: 'ed@leijnse.info' };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -35,7 +35,7 @@ export default class Login extends React.Component {
     handleSubmit (event) {
         const templateId = 'template_mij17qc';
 
-        this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
+        this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.from_name, to_name: this.state.to_name,  reply_to: this.state.email})
     }
 
     sendFeedback (templateId, variables) {
